04a803f2e22d8d50846601be1c4e5e56222c09ea
young na <ygna91@gmail.com>
2019-11-14 15:16:47 +0900

    Refactor : express-generator로 생성된 불필요 코드/파일 삭제
    상세 : router폴더 삭제, routing 로직 삭제, 쿠키파서 미들웨어 적용부분 삭제
    이유 : 필요하지 않은 디펜던시를 줄여서 안정성을 높이고 가볍게 유지하기 위해

--- server/app.js
+++ server/app.js
@@ -3,18 +3,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({
+  extended: false,
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
