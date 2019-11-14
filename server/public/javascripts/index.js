e7b11a1789138101ab1f872186d62d475f278cba
Mosball <rnjsrldnd123@naver.com>
2019-11-14 17:41:29 +0900

    Feature : socket.io 이벤트 핸들러 스켈레톤 코드 구현
    상세 : sdp, ice, user-left, user-joined, connect
    이벤트에 대한 핸들러를 스켈레톤 코드로 구현
    
    close #106

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -1,12 +1,20 @@
 const localVideo = document.querySelector('.local-video');
+const rtcPeerConnections = [];
 let localStream;
+let socketId;
 
 const mediaConstraints = { video: true, audio: false };
 
-const getUserMediaSuccess = (stream) => {
+const getUserMediaSuccess = stream => {
   localStream = stream;
   localVideo.srcObject = stream;
 };
+
+const getSdpFromServer = () => {};
+const getIceFromServer = () => {};
+const userLeftHandler = () => {};
+const userJoinHandler = () => {};
+const initSocketId = () => {};
 
 const runWebRTC = async () => {
   if (!navigator.mediaDevices.getUserMedia) {
@@ -15,6 +23,14 @@ const runWebRTC = async () => {
     const stream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
     getUserMediaSuccess(stream);
   }
+
+  socket = io();
+
+  socket.on('sdp', getSdpFromServer);
+  socket.on('ice', getIceFromServer);
+  socket.on('user-left', userLeftHandler);
+  socket.on('user-joined', userJoinHandler);
+  socket.on('connect', initSocketId);
 };
 
 runWebRTC();
