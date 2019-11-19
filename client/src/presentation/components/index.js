83cdcd19e9518cd4d69ab774298d5123ee59d888
JeongHyeonJo <zo13258@naver.com>
2019-11-19 21:30:55 +0900

    Feature : description 컴포넌트 구현
    상세 : p태그의 마진을 변경하여 description 컴포넌트 구현
    이유 : p태그를 그대로 사용할 경우, 예상하지 않은 마진이 생김
    
    close #169

--- client/src/presentation/components/index.js
+++ client/src/presentation/components/index.js
@@ -2,5 +2,6 @@ import Logo from './Logo';
 import TextInput from './TextInput';
 import { SmallButton, LargeButton } from './Buttons';
 import Title from './Title';
+import Description from './Description';
 
-export { Logo, TextInput, SmallButton, LargeButton, Title };
+export { Logo, TextInput, SmallButton, LargeButton, Title, Description };
