65e291727a267155d12735a3dc9f02b65c7a1ad2
JeongHyeonJo <zo13258@naver.com>
2019-11-19 14:16:36 +0900

    Feature : main title 컨테이너 구현
    상세 : logo 컴포넌트를 포함하는 title 컨테이너
    styled component를 이용해 구조와 css 구현
    
    close #152

---
+++ client/src/presentation/containers/main-title.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../components';
+import logoSrc from '../../assets/main-logo-large.png';
+
+const TitleWrapper = styled.div`
+  heigth: 100%;
+  width: 100%;
+`;
+
+const MainTitle = () => {
+  return (
+    <TitleWrapper>
+      <Logo logoSrc={logoSrc} />
+    </TitleWrapper>
+  );
+};
+
+export default MainTitle;
