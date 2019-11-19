b3576ef1ac8e590c11552f71668ce666acfa4efe
Mosball <rnjsrldnd123@naver.com>
2019-11-19 14:28:42 +0900

    Feature : Ready한 사용자의 Socket id를 저장 및 삭제하는 기능 구현
    상세 : 시그널 서버에서 ready이벤트를 전달받을 경우
    isReady플래그 값에 따라 room.readyUsers에
    socket.id를 저장 또는 삭제함
    
    close #153

--- server/app.js
+++ server/app.js
@@ -20,6 +20,17 @@ io.on('connection', socket => {
   socket.on('join', ({ roomNumber }) => {
     socket.join(roomNumber);
   });
+
+  socket.on('ready', ({ isReady }) => {
+    const [roomNumber] = Object.keys(socket.rooms);
+    const room = io.sockets.adapter.rooms[roomNumber];
+
+    if (isReady) {
+      room.readyUsers[socket.id] = true;
+    } else {
+      delete room.readyUsers[socket.id];
+    }
+  });
 });
 app.io = io;
 
