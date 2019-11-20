083348e876884c9fe1d70bd32b97e73a2b89a8ec
OriginJang <jkwkw17@gmail.com>
2019-11-20 17:09:09 +0900

    Refactor : menu container 리팩토링
    상세:
    - input text, large button을 container로 감싸서 구현
    
    close #183

--- client/src/presentation/containers/Menu.jsx
+++ client/src/presentation/containers/Menu.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { LargeButton, TextInput } from '../components';
 
-const MenuWrapper = styled.div`
-  margin-top: 2rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: white;
-  box-shadow: 0rem 0.1rem 0.1rem 0rem grey;
-  height: 10rem;
-  justify-content: space-evenly;
-`;
+const useStyle = makeStyles({
+  menu: {
+    width: '100%',
+    height: 220,
+    padding: '20px',
+    border: '1px solid #cccccc',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    borderRadius: 5,
+    boxShadow:
+      '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
+  },
+});
 
 const textInpuStyles = {
   width: '100%',
 };
 
 const Menu = () => {
+  const classes = useStyle();
+
   return (
-    <MenuWrapper>
+    <Container maxWidth="md" className={classes.menu}>
       <TextInput label="NAME" id="user-name" style={textInpuStyles} />
       <LargeButton text="PLAY" onClick={() => {}} />
       <LargeButton text="RANK" onClick={() => {}} />
-    </MenuWrapper>
+    </Container>
   );
 };
 
