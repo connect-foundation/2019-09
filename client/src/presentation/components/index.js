22a94797a6adc6cf3aa6590e54c9a4df5302b5e1
young na <ygna91@gmail.com>
2019-11-20 20:39:39 +0900

    Feature : QuizDisplay를 구현한 후 Game 페이지에 적용
    상세:
    - word를 받아 한 글자씩 UnderlinedSpace와
      UnderlinedLetter로 만든 후 사이에 공간을 넣음
    - 단어가 secret인지 아닌지를 받아 위의 상황을 분기
    - Game 페이지의 상단의 중앙에 center로 정렬하여 display
    
    close #200

--- client/src/presentation/components/index.js
+++ client/src/presentation/components/index.js
@@ -4,5 +4,15 @@ import { SmallButton, LargeButton } from './Buttons';
 import Title from './Title';
 import Description from './Description';
 import Timer from './Timer';
+import QuizDisplay from './QuizDisplay';
 
-export { Logo, TextInput, SmallButton, LargeButton, Title, Description, Timer };
+export {
+  Logo,
+  TextInput,
+  SmallButton,
+  LargeButton,
+  Title,
+  Description,
+  Timer,
+  QuizDisplay,
+};
--- client/src/presentation/pages/Game.jsx
+++ client/src/presentation/pages/Game.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
-import { Timer } from '../components';
+import { Timer, QuizDisplay } from '../components';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,8 +29,10 @@ const Game = () => {
             <Timer currentSeconds="120" />
           </Box>
         </Grid>
-        <Grid item xs>
-          <Box className={classes.paper}>xs</Box>
+        <Grid item xs justify="center" alignItems="center" container>
+          <Box className={classes.paper}>
+            <QuizDisplay word="hello" isSecret />
+          </Box>
         </Grid>
         <Grid item xs>
           <Box className={classes.paper}>xs</Box>
