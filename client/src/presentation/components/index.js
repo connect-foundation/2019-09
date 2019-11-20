846df9e657138227e0a623f580a82bfddc970fe4
young na <ygna91@gmail.com>
2019-11-20 20:02:00 +0900

    Feature : game페이지내 timer 구현
    상세:
    - Timer 컴포넌트 구현, props에서 currentCount 받아서 표현하도록 구현
    - Game page에서 Timer 컴포넌트 가져와 사용하도록 구현
    
    close #197

--- client/src/presentation/components/index.js
+++ client/src/presentation/components/index.js
@@ -3,5 +3,6 @@ import TextInput from './TextInput';
 import { SmallButton, LargeButton } from './Buttons';
 import Title from './Title';
 import Description from './Description';
+import Timer from './Timer';
 
-export { Logo, TextInput, SmallButton, LargeButton, Title, Description };
+export { Logo, TextInput, SmallButton, LargeButton, Title, Description, Timer };
--- client/src/presentation/pages/Game.jsx
+++ client/src/presentation/pages/Game.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
+import { Timer } from '../components';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
+  timerBox: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+  },
+  gameHeader: {
+    backgroundColor: '#5a96ff',
   },
 }));
 
@@ -19,26 +23,28 @@ const Game = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
+      <Grid container spacing={0} className={classes.gameHeader}>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Box className={classes.timerBox}>
+            <Timer currentSeconds="120" />
+          </Box>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Box className={classes.paper}>xs</Box>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Box className={classes.paper}>xs</Box>
         </Grid>
       </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+      <Grid container spacing={0}>
+        <Grid item xs={2}>
+          <Box className={classes.paper}>xs</Box>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <Box className={classes.paper}>xs=6</Box>
         </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+        <Grid item xs={4}>
+          <Box className={classes.paper}>xs</Box>
         </Grid>
       </Grid>
     </div>
