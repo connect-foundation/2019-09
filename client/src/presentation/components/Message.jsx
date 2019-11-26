8e12972106fc2f17633c800390565eab6ae2145a
OriginJang <jkwkw17@gmail.com>
2019-11-26 15:22:27 +0900

    Fix : Message 컴포넌트 수정
    상세 :
    - message props를 children으로 변경
    - font size 지정

--- client/src/presentation/components/Message.jsx
+++ client/src/presentation/components/Message.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  message: {},
+  message: {
+    fontSize: '1.4rem',
+  },
 });
 
-const Message = ({ message }) => {
+const Message = ({ children }) => {
   const classes = useStyles();
   return (
     <Box component="span" className={classes.message}>
-      {message}
+      {children}
     </Box>
   );
 };
 
 Message.propTypes = {
-  message: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 };
 
 export default Message;
