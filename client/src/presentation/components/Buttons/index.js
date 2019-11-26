47adf0d543b581eba7b83a4965ffca20dd0c36d5
OriginJang <jkwkw17@gmail.com>
2019-11-26 17:44:02 +0900

    Feature : SendButton 컴포넌트 구현
    상세 :
    - input window에서 사용할 버튼 컴포넌트 생성
    - 버튼의 공통 스타일을  /Buttons/style.js 모듈화함
    
    이유:
    - 외부에서 스타일을 주입받아서 컴포넌트에 적용하는데 번거로움을 느낌
    - 코드리뷰에서 필요한 컴포넌트들을 미리 구현해놓는 것을 추천받음
    
    close #252

--- client/src/presentation/components/Buttons/index.js
+++ client/src/presentation/components/Buttons/index.js
@@ -1,2 +1,3 @@
 export { default as SmallButton } from './SmallButton';
 export { default as LargeButton } from './LargeButton';
+export { default as SendButtton } from './SendButtton';
---
+++ client/src/presentation/components/Buttons/style.js
@@ -0,0 +1,12 @@
+const buttonStyle = {
+  background: '#5A96FF',
+  border: 0,
+  borderRadius: 3,
+  color: 'white',
+  padding: '0 30px',
+  '&:hover': {
+    background: '#497FDB',
+  },
+};
+
+export default buttonStyle;
