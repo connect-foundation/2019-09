fe5dc6cef7263ac4f8544cacccd67c1e09e73ea2
young na <ygna91@gmail.com>
2019-11-27 22:05:07 +0900

    Feature : 서버에서 sendDescription과 sendCandidate를 relay하도록 구현
    상세:
    - 두 이벤트 모두 targetSocketId에게 description 혹은 candidate를 relay 하는
      기능 구현

--- server/socket/index.js
+++ server/socket/index.js
@@ -14,6 +14,14 @@ io.on('connection', socket => {
   socket.on('sendReady', sendReadyHandler.bind(null, socket));
 
   socket.on('disconnecting', disconnectingHandler.bind(null, socket));
+
+  socket.on('sendDescription', ({ target, description }) => {
+    io.to(target).emit('sendDescription', { target: socket.id, description });
+  });
+
+  socket.on('sendCandidate', ({ target, candidate }) => {
+    io.to(target).emit('sendCandidate', { target: socket.id, candidate });
+  });
 });
 
 module.exports = io;
