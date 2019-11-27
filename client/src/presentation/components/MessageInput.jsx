38c08597a8c07cc091dd0b94890507bdcd829cb3
OriginJang <jkwkw17@gmail.com>
2019-11-27 16:55:17 +0900

    Feature : MessageInput props 수정
    상세 :
    - props로 input value와 onChangeHandler를 받음
    - 받아온 props를 Input에 주입
    
    close #270

--- client/src/presentation/components/MessageInput.jsx
+++ client/src/presentation/components/MessageInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 
@@ -8,10 +9,14 @@ const useStyles = makeStyles({
     flex: 8,
   },
 });
-const MessageInput = () => {
+const MessageInput = ({ value, valueChangeHandler }) => {
   const classes = useStyles();
   return (
     <Input
+      value={value}
+      onChange={e => {
+        valueChangeHandler(e.target.value);
+      }}
       placeholder="Please enter a message."
       className={classes.input}
       inputProps={{
@@ -19,6 +24,11 @@ const MessageInput = () => {
       }}
     />
   );
+};
+
+MessageInput.propTypes = {
+  valueChangeHandler: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
 };
 
 export default MessageInput;
