3877e370bbc1160eb9a8e7e66124bec2753111be
Mosball <rnjsrldnd123@naver.com>
2019-11-27 13:51:29 +0900

    Feature : ChattingWindow에서 사용될 reducer 구현
    상세 :
    - newChatting : 새로운 채팅을 추가할 때 사용
    - resetChattingList : 모든 채팅을 초기화할 때 사용
    
    close #264

--- client/src/store/globalReducer.js
+++ client/src/store/globalReducer.js
@@ -2,6 +2,16 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'changeRoomId':
       return { ...state, roomId: action.payload.roomId };
+    case 'newChatting':
+      return {
+        ...state,
+        chattingList: [...state.chattingList, action.payload.newChat],
+      };
+    case 'resetChattingList':
+      return {
+        ...state,
+        chattingList: [],
+      };
     default:
       throw new Error();
   }
