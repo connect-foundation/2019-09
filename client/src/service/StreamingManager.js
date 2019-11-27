103ada4f2cdf598ad0c1f29f4a064784ac727823
young na <ygna91@gmail.com>
2019-11-27 22:08:49 +0900

    Feature : 자신이 스트리머일시 local의 스트림을 video 태그에 넣는 기능 추가
    상세:
    - 뷰와 연결하기전 테스트를 위해 직접적으로
    video 트랙을 video 태그의 srcObject에 추가하여 스트리머가 자신을 볼 수 있도록
    기능 구현
    - 현재 구현되지 않은 view에서 전달할 dispatch는 임시로 주석처리 함

--- client/src/service/StreamingManager.js
+++ client/src/service/StreamingManager.js
@@ -22,15 +22,14 @@ class StreamingManager {
       webRTCManager,
       iceCandidateHandler,
       socket,
-      dispatch,
+      // dispatch,
       remotePlayers,
     } = this;
     const socketIds = Object.keys(remotePlayers);
-
     webRTCManager.closeAllConnections();
     await webRTCManager.createStream();
     // 자신의 로컬 스트림을 생성하면 View로 dispatch 추후 디스플레이여부는 View에서 관장
-    dispatch({ type: 'setStream', payload: { stream: webRTCManager.stream } });
+    // dispatch({ type: 'setStream', payload: { stream: webRTCManager.stream } });
     webRTCManager.createConnections(socketIds);
     webRTCManager.registerIceCandidates(
       socketIds,
@@ -45,6 +44,8 @@ class StreamingManager {
         description: offers[index],
       });
     });
+    const stream = webRTCManager.getStream();
+    document.querySelector('video').srcObject = stream;
   }
 
   async assignViewerHandler({ streamerSocketId }) {
@@ -70,6 +71,7 @@ class StreamingManager {
   }
 
   iceCandidateHandler(socketId, event) {
+    if (!event.candidate) return;
     this.socket.emit('sendCandidate', {
       target: socketId,
       candidate: event.candidate,
@@ -77,7 +79,8 @@ class StreamingManager {
   }
 
   trackHandler(stream) {
-    this.dispatch({ type: 'setStream', payload: { stream } });
+    document.querySelector('video').srcObject = stream;
+    // this.dispatch({ type: 'setStream', payload: { stream } });
   }
 }
 
