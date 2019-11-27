4fbc2e039afb0c16a7b52fb27c96b5afb33ac87c
young na <ygna91@gmail.com>
2019-11-27 21:10:18 +0900

    Feature : assignViewer와 assignStreamer 이벤트 발생 구현
    상세:
    - 스트리머와 뷰어를 정하여 클라이언트로 해당 정보를 전송하는
     함수를 구현
    
    #279

--- server/socket/handlers/sendReadyHandler.js
+++ server/socket/handlers/sendReadyHandler.js
@@ -1,17 +1,43 @@
 const rooms = require('../rooms');
 const io = require('../io');
 
-const startGame = (socket, roomId) => {
-  rooms.resetGameProgress(roomId);
-  io.to(roomId).emit('gameStart');
+const assignViewer = socket => {
+  const room = rooms.getRoomByRoomId(socket.roomId);
+  const { streamerSocketId } = room;
+  const socketIds = Object.keys(room.players);
+  socketIds.forEach(socketId => {
+    if (socketId !== streamerSocketId) {
+      io.to(socketId).emit('assignViewer', { streamerSocketId });
+    }
+  });
+};
+
+const assignStreamer = socket => {
+  const { streamerSocketId } = rooms.getRoomByRoomId(socket.roomId);
+  io.to(streamerSocketId).emit('assignStreamer');
+};
+
+const startRound = roomId => {
   rooms.setRound(roomId);
   io.to(roomId).emit('roundStart', {
     currentRound: rooms.getRoomByRoomId(roomId).currentRound,
   });
+};
+
+const startSet = roomId => {
   rooms.setSet(roomId);
   io.to(roomId).emit('setStart', {
     currentSet: rooms.getRoomByRoomId(roomId).currentSet,
   });
+};
+
+const startGame = (socket, roomId) => {
+  rooms.resetGameProgress(roomId);
+  io.to(roomId).emit('gameStart');
+  startRound(roomId);
+  startSet(roomId);
+  assignStreamer(socket);
+  assignViewer(socket);
 };
 
 const setPlayerReady = (socket, isReady) => {
