aa30e529fd9b53a457dbc14c07d8a4e2edf1b8a6
young na <ygna91@gmail.com>
2019-12-05 00:19:49 +0900

    Feature : 랭킹 페이지에서 보이는 세 단상의 단상 컴포넌트 구현
    상세:
    - 등수를 받아 높이를 조절하도록 구현
    - styled component를 이용하여 구현
    - 수상자의 정보를 받아 나타내도록 구현
    - 추후 TopRankPanel에서 사용할 예정

---
+++ client/src/presentation/components/RankPodium.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { STYLE_COLORS } from '../../utils';
+
+const RankPodiumBar = styled.div`
+  height: ${props => {
+    if (props.rank === '1') return '75%';
+    if (props.rank === '2') return '50%';
+    return '25%';
+  }};
+  border: 2px solid ${STYLE_COLORS.THEME_COLOR};
+  display: flex;
+  font-size: 5rem;
+  flex-direction: colun;
+  justify-content: center;
+  align-items: center;
+  background-color: ${STYLE_COLORS.PURE_WHITE_COLOR};
+  color: ${STYLE_COLORS.THEME_COLOR};
+  font-weight: bold;
+`;
+
+const RankPodiumWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const RankerInformation = styled.div`
+  height: ${props => {
+    if (props.rank === '1') return '25%';
+    if (props.rank === '2') return '50%';
+    return '75%';
+  }};
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const RankerDescription = styled.div`
+  font-size: 2.5rem;
+  text-align: center;
+`;
+
+const RankPodium = ({ rank, rankerScore, rankerNickname }) => {
+  return (
+    <RankPodiumWrapper>
+      <RankerInformation rank={rank}>
+        <RankerDescription>
+          <div>{rankerNickname}</div>
+          <div>{rankerScore}</div>
+        </RankerDescription>
+      </RankerInformation>
+      <RankPodiumBar rank={rank}>{rank}</RankPodiumBar>
+    </RankPodiumWrapper>
+  );
+};
+
+RankPodium.propTypes = {
+  rank: PropTypes.string.isRequired,
+  rankerScore: PropTypes.number.isRequired,
+  rankerNickname: PropTypes.string.isRequired,
+};
+
+export default RankPodium;
