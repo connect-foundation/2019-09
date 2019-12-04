f976c80d755e5ecfb19f4abfd39960314dc5fc33
JeongHyeonJo <zo13258@naver.com>
2019-12-05 02:19:48 +0900

    Feature : sequelize 데이터베이스 연결 설정
    상세 : 데이터베이스 연결에 필요한 사용자 아이디, 비밀번호 등
    관련 정보를 dotenv를 활용해서 등록
    
    close #334

---
+++ server/databaseModels/connection.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+const { env } = process;
+
+const connection = new Sequelize.Sequelize(
+  env.MYSQL_DATABASE,
+  env.MYSQL_USER_NAME,
+  env.MYSQL_USER_PASSWORD,
+  {
+    host: env.DATABASE_HOST,
+    dialect: env.DATABASE_DIALECT,
+    charset: env.DATABASE_CHARSET,
+    collate: env.DATABASE_COLLATE,
+    pool: {
+      max: 10000,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+);
+
+module.exports = connection;
