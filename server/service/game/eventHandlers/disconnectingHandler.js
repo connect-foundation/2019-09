42fa20ff54754a994d69106ad9ed0dcb36511756
JeongHyeonJo <zo13258@naver.com>
2019-12-08 16:26:24 +0900

    Fix : 방에서 레디하고 있는 사람만 남을 경우, 게임 시작 기능 구현
    상세 :
    - sendReadyHandler 코드 내용 재활용
    - 나갔는데 게임을 진행하기 가능한 상황이면 게임을 바로 실행
    
    close #369

--- server/service/game/eventHandlers/disconnectingHandler.js
+++ server/service/game/eventHandlers/disconnectingHandler.js
@@ -2,6 +2,67 @@ const { io } = require('../../io');
 const roomController = require('../roomController');
 const { MIN_PLAYER_COUNT } = require('../../../config');
 
+const assignStreamer = streamer => {
+  io.to(streamer.socketId).emit('assignStreamer');
+};
+
+const assignViewer = (viewer, streamer) => {
+  io.to(viewer.socketId).emit('assignViewer', {
+    streamerSocketId: streamer.socketId,
+  });
+};
+const sendReady = (roomId, player) => {
+  io.in(roomId).emit('sendReady', {
+    socketId: player.getSocketId(),
+    isReady: player.getIsReady(),
+  });
+};
+
+const isRoomReady = gameManager => {
+  const players = gameManager.getPlayers();
+  return players.every(player => player.getIsReady());
+};
+
+const assignPlayerType = gameManager => {
+  const streamer = gameManager.getStreamer();
+  const viewers = gameManager.getOtherPlayers(streamer.getSocketId());
+
+  assignStreamer(streamer);
+  viewers.forEach(viewer => {
+    assignViewer(viewer, streamer);
+  });
+};
+
+// const emit
+
+const disconnectPlayersAndStartGame = (players, gameManager) => {
+  players.forEach(player => {
+    const socket = io.sockets.connected[player.getSocketId()];
+    socket.disconnect();
+    // disconnectingHandler에서 gameManager의 player를 지워줘야함
+  });
+  /**
+   * 연결준비 후 응답이 없는 플레이어를 제외하고 시작
+   */
+  gameManager.getPlayers().forEach(player => {
+    const socketId = player.getSocketId();
+
+    io.to(socketId).emit('prepareSet', {
+      currentRound: gameManager.getCurrentRound(),
+      currentSet: gameManager.getCurrentSet(),
+      quizCandidates: gameManager.isStreamer(socketId)
+        ? ['문어', '고양이', '부스트캠퍼']
+        : [], // getQuizCandidates()
+    });
+  });
+
+  gameManager.startQuizSelectTimer(currentSeconds => {
+    io.in(gameManager.getRoomId()).emit('sendCurrentSeconds', {
+      currentSeconds,
+    });
+  });
+};
+
 const disconnectingHandler = socket => {
   try {
     const { gameManager } = roomController.getRoomByRoomId(socket.roomId);
@@ -12,7 +73,24 @@ const disconnectingHandler = socket => {
       socketId: socket.id,
     });
 
-    if (gameManager.getStatus() === 'waiting') return;
+    if (gameManager.getStatus() === 'waiting') {
+      const playerCount = gameManager.getPlayers().length;
+
+      if (isRoomReady(gameManager) && playerCount >= MIN_PLAYER_COUNT) {
+        gameManager.cancelReadyAllPlayers();
+        gameManager
+          .getPlayers()
+          .forEach(sendReady.bind(null, gameManager.getRoomId()));
+
+        gameManager.prepareGame();
+        io.in(gameManager.getRoomId()).emit('startGame');
+
+        gameManager.prepareSet();
+        assignPlayerType(gameManager);
+        gameManager.startPeerConnectCheckTimer(disconnectPlayersAndStartGame);
+        return;
+      }
+    }
     if (gameManager.getStatus() === 'initializing') {
       if (
         !gameManager.getStreamer() ||
