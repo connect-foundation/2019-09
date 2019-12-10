3294e4245f297b3981553a916fb6b6049033d5e6
OriginJang <jkwkw17@gmail.com>
2019-12-10 17:53:01 +0900

    Feature : Ranking seeder 구현
    상세 :
    - bultCreate를 통해 여러 row를 한번에 insert
    - Quiz도 bultCreate로 변경
    
    이유 :
    - create로 하나씩 insert를 하면 connection이 하나의 insert 쿼리 마다 생김
    
    #383

--- server/databaseFiles/databaseModels/seeder/index.js
+++ server/databaseFiles/databaseModels/seeder/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const csv = require('csv-parser');
-const { Quiz } = require('../');
+const { Quiz, Ranking } = require('../');
 
 const initializeQuizzes = async () => {
   const quizzes = [];
@@ -11,13 +11,31 @@ const initializeQuizzes = async () => {
     })
     .on('end', async () => {
       try {
-        quizzes.forEach(async quiz => {
-          await Quiz.create(quiz);
-        });
+        await Quiz.bulkCreate(quizzes);
       } catch (error) {
         console.error(error);
       }
     });
 };
 
-module.exports = { initializeQuizzes };
+const initializeRanking = async () => {
+  const rankings = [];
+  fs.createReadStream('./databaseFiles/databaseModels/seeder/ranking.csv')
+    .pipe(csv())
+    .on('data', data => {
+      rankings.push(data);
+    })
+    .on('end', async () => {
+      try {
+        const rankingDummyList = rankings.map(ranking => {
+          ranking.score = Math.round(Math.random() * 1000);
+          return ranking;
+        });
+        await Ranking.bulkCreate(rankingDummyList);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+};
+
+module.exports = { initializeQuizzes, initializeRanking };
