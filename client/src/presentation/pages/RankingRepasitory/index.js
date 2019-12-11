92043c6827858b2304f88d159e12c057e7aade14
OriginJang <jkwkw17@gmail.com>
2019-12-11 20:29:37 +0900

    Feature : 무한 스크롤 구현
    상세 :
    - 최초 렌더링 시 랭킹데이터 요청
    - 스크롤을 가장 아래로 내리면 offset 변경
      - offset이 변경되면 서버에 랭킹 데이터 요청 후 재렌더링
    - 서버 포트 수정
    
     #384

--- client/src/presentation/pages/RankingRepasitory/index.js
+++ client/src/presentation/pages/RankingRepasitory/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '@material-ui/core/Box';
 import { Link } from 'react-router-dom';
 import { TopRankPanel, BottomRankPanel } from '../../containers';
 import { ExitButton } from '../../components';
 import useStyle from './style';
-import { getRankingsTop100 } from '../../../api';
+import { getRankings } from '../../../api';
 
 const getTopRankingList = totalRankingList => {
   return totalRankingList.slice(0, 3);
@@ -14,32 +14,62 @@ const getBottomRankingList = totalRankingList => {
   return totalRankingList.slice(3);
 };
 
-const setRankingLists = async (setTopRankingList, setBottomRankingList) => {
-  const rankingsData = await getRankingsTop100();
-  const rankingList = rankingsData.map((ranking, index) => {
-    return {
-      rank: `${index + 1}`,
-      nickname: ranking.nickname,
-      score: `${ranking.score}`,
-    };
-  });
-
-  setTopRankingList(getTopRankingList(rankingList));
-  setBottomRankingList(getBottomRankingList(rankingList));
-};
-
 const Ranking = () => {
   const classes = useStyle();
-
+  const [offset, setOffset] = useState(0);
+  const [pageHeight, setPageHeight] = useState(0);
+  const [scrollLock, setScrollLock] = useState(false);
   const [topRankingList, setTopRankingList] = useState([]);
   const [bottomRankingList, setBottomRankingList] = useState([]);
+  const ref = useRef();
+
+  const isScrollBottom = () =>
+    ref.current.offsetHeight + ref.current.scrollTop ===
+    ref.current.scrollHeight;
+  const isPageHeightChanged = () => pageHeight !== ref.current.scrollHeight;
+
+  /**
+   * offset이 변경되면 서버에 랭킹 데이터 요청 후 렌더링
+   */
+  const setRankingLists = async () => {
+    setScrollLock(true);
+    const rankingsData = await getRankings(offset);
+    const rankingList = rankingsData.map((ranking, index) => {
+      return {
+        rank: `${index + 1 + topRankingList.length + bottomRankingList.length}`,
+        nickname: ranking.nickname,
+        score: `${ranking.score}`,
+      };
+    });
+
+    if (offset === 0) {
+      setTopRankingList(getTopRankingList(rankingList));
+      setBottomRankingList(getBottomRankingList(rankingList));
+    } else {
+      setBottomRankingList([...bottomRankingList, ...rankingList]);
+    }
+    setScrollLock(false);
+  };
+  /**
+   * 스크롤이 가장 아래로 내려갔을때, offset을 변경하고 컴포넌트 높이를 업데이트
+   */
+  const onScrollHandler = () => {
+    if (isScrollBottom() && isPageHeightChanged() && !scrollLock) {
+      setOffset(offset + 1);
+      setPageHeight(ref.current.scrollHeight);
+    }
+  };
 
   useEffect(() => {
-    setRankingLists(setTopRankingList, setBottomRankingList);
-  }, []);
+    setRankingLists();
+  }, [offset]);
 
   return (
-    <Box className={classes.mainPageWrapper}>
+    <Box
+      className={classes.mainPageWrapper}
+      ref={ref}
+      onScroll={onScrollHandler}
+    >
       <Box className={classes.exitButtonWrapper}>
         <Link to="/">
           <ExitButton />
