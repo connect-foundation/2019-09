d60303423c24fc8d978947469afabfc4a874bf1e
Mosball <rnjsrldnd123@naver.com>
2019-12-12 11:38:06 +0900

    Fix : 각 페이지에 적용되있던 background-image속성을 상위로 이동
    상세 : Main, Game, Ranking 페이지에 적용되있던
    background-image css속성을 App.jsx를 감싸는
    styled component쪽으로 이동
    
    이유 : 각 페이지에 background-image가 적용될 경우
    페이지가 리렌더링 될 때 이미지가 깜빡거리는 현상이 발생함
    
    close #423

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-cycle
+import styled from 'styled-components';
 import App from './App';
+import backgroundImageSource from './assets/background.png';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppBackgroundWrapper = styled.div`
+  height: 100%;
+  background-image: url(${backgroundImageSource});
+`;
+
+ReactDOM.render(
+  <AppBackgroundWrapper>
+    <App />
+  </AppBackgroundWrapper>,
+  document.getElementById('root'),
+);
