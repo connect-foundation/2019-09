a555dc4cecfd20ebe15c568121ae9983a65a472d
OriginJang <jkwkw17@gmail.com>
2019-12-12 19:56:54 +0900

    Feature : 랭킹데이터 요청 더보기로 구현
    상세 :
    - More 버튼 클릭 시 offset을 하나 증가시키고 offset이 증가됨에 따라
      랭킹데이터 요청
    - 랭킹데이터 요청 후, 0.5초 동안 데이터요청 방지
    
    #437

--- client/src/presentation/pages/Ranking/index.js
+++ client/src/presentation/pages/Ranking/index.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import { Link } from 'react-router-dom';
+import { MoreButton } from '../../components/Buttons';
 import { TopRankPanel, BottomRankPanel } from '../../containers';
 import { ExitButton } from '../../components';
 import useStyle from './style';
@@ -17,22 +18,15 @@ const getBottomRankingList = totalRankingList => {
 const Ranking = () => {
   const classes = useStyle();
   const [offset, setOffset] = useState(0);
-  const [pageHeight, setPageHeight] = useState(0);
-  const [scrollLock, setScrollLock] = useState(false);
+  const [moreButtonLock, setMoreButtonLock] = useState(false);
   const [topRankingList, setTopRankingList] = useState([]);
   const [bottomRankingList, setBottomRankingList] = useState([]);
-  const ref = useRef();
-
-  const isScrollBottom = () =>
-    ref.current.offsetHeight + ref.current.scrollTop ===
-    ref.current.scrollHeight;
-  const isPageHeightChanged = () => pageHeight !== ref.current.scrollHeight;
 
   /**
    * offset이 변경되면 서버에 랭킹 데이터 요청 후 렌더링
    */
   const setRankingLists = async () => {
-    setScrollLock(true);
+    setMoreButtonLock(true);
     const rankingsData = await getRankings(offset);
     const rankingList = rankingsData.map((ranking, index) => {
       return {
@@ -41,23 +35,20 @@ const Ranking = () => {
         score: `${ranking.score}`,
       };
     });
-
-    if (offset === 0) {
-      setTopRankingList(getTopRankingList(rankingList));
-      setBottomRankingList(getBottomRankingList(rankingList));
-    } else {
-      setBottomRankingList([...bottomRankingList, ...rankingList]);
-    }
-    setScrollLock(false);
+    setTimeout(() => {
+      if (offset === 0) {
+        setTopRankingList(getTopRankingList(rankingList));
+        setBottomRankingList(getBottomRankingList(rankingList));
+      } else {
+        setBottomRankingList([...bottomRankingList, ...rankingList]);
+      }
+      setMoreButtonLock(false);
+    }, 500);
   };
-  /**
-   * 스크롤이 가장 아래로 내려갔을때, offset을 변경하고 컴포넌트 높이를 업데이트
-   */
-  const onScrollHandler = () => {
-    if (isScrollBottom() && isPageHeightChanged() && !scrollLock) {
-      setOffset(offset + 1);
-      setPageHeight(ref.current.scrollHeight);
-    }
+
+  const onClick = () => {
+    if (moreButtonLock) return;
+    setOffset(offset + 1);
   };
 
   useEffect(() => {
@@ -65,18 +56,20 @@ const Ranking = () => {
   }, [offset]);
 
   return (
-    <Box
-      className={classes.mainPageWrapper}
-      ref={ref}
-      onScroll={onScrollHandler}
-    >
+    <Box className={classes.mainPageWrapper}>
       <Box className={classes.exitButtonWrapper}>
         <Link to="/">
           <ExitButton />
         </Link>
       </Box>
       <TopRankPanel rankingList={topRankingList} />
-      <BottomRankPanel rankingList={bottomRankingList} />
+      <BottomRankPanel
+        rankingList={bottomRankingList}
+        loading={moreButtonLock}
+      />
+      <Box className={classes.MoreButton}>
+        <MoreButton onClick={onClick}>MORE</MoreButton>
+      </Box>
     </Box>
   );
 };
