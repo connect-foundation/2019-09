54171b05b45d0e67319ad777b4d48d72dd719d30
JeongHyeonJo <zo13258@naver.com>
2019-12-12 11:39:32 +0900

    FIx : 스트리머 여부 확인 추가
    상세 :
    - 스트리머인지 확인하는 부분 추가
    
    #406

--- server/service/game/eventHandlers/selectQuizHandler.js
+++ server/service/game/eventHandlers/selectQuizHandler.js
@@ -19,11 +19,13 @@ const isQuizInQuizCandidates = (quizCandidates, quiz) => {
 const selectQuizHandler = (socket, { quiz }) => {
   const { gameManager, timer } = roomController.getRoomByRoomId(socket.roomId);
   /**
+   * 전송자가 스트리머인지,
    * 게임의 상태가 단어 선택 단계인지,
    * 퀴즈의 단어가 선택됐는지,
    * 마지막으로 퀴즈의 단어가 목록에 있는지 확인하는 로직
    */
   if (
+    gameManager.isStreamer(socket.id) &&
     isGameInitializing(gameManager.getStatus()) &&
     quizNotSelected(gameManager.getQuiz()) &&
     isQuizInQuizCandidates(gameManager.getQuizCandidates(), quiz)
