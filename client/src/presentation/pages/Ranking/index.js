68c83435b3f6876641008fc2cf8ce71ede1b752f
OriginJang <jkwkw17@gmail.com>
2019-12-12 23:18:17 +0900

    Feature : 데이터 무결성 보장과 더보기 버튼 제거 기능 구현
    상세 :
    - 데이터 무결성을 보장하기 위해 서버 시간을 가져옴
      - 요청한 데이터를 기준으로 이전의 랭킹 정보를 가져옴
    - 모든 데이터를 가져왔을때, 더보기 버튼을 제거함
      - 데이터의 모든 수를 알기 위해 첫번째 렌더링 시 랭킹 레코드 수를 가져옴
    
    close #443

--- client/src/presentation/pages/Ranking/index.js
+++ client/src/presentation/pages/Ranking/index.js
@@ -5,7 +5,7 @@ import { MoreButton } from '../../components/Buttons';
 import { TopRankPanel, BottomRankPanel } from '../../containers';
 import { ExitButton } from '../../components';
 import useStyle from './style';
-import { getRankings } from '../../../api';
+import { getRankings, getRankingInformation } from '../../../api';
 
 const getTopRankingList = totalRankingList => {
   return totalRankingList.slice(0, 3);
@@ -18,16 +18,28 @@ const getBottomRankingList = totalRankingList => {
 const Ranking = () => {
   const classes = useStyle();
   const [offset, setOffset] = useState(0);
+  const [rankingCount, setRankingCount] = useState(0);
+  const [currentDateTime, setCurrentDateTime] = useState('');
   const [moreButtonLock, setMoreButtonLock] = useState(false);
   const [topRankingList, setTopRankingList] = useState([]);
   const [bottomRankingList, setBottomRankingList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [moreButtomVisibiliy, setMoreButtomVisibiliy] = useState(true);
 
+  const isAllRankingsFetched = () => {
+    return (
+      rankingCount !== 0 &&
+      rankingCount <= bottomRankingList.length + topRankingList.length
+    );
+  };
   /**
    * offset이 변경되면 서버에 랭킹 데이터 요청 후 렌더링
    */
   const setRankingLists = async () => {
     setMoreButtonLock(true);
-    const rankingsData = await getRankings(offset);
+    setLoading(true);
+
+    const rankingsData = await getRankings(offset, currentDateTime);
     const rankingList = rankingsData.map((ranking, index) => {
       return {
         rank: `${index + 1 + topRankingList.length + bottomRankingList.length}`,
@@ -35,13 +47,26 @@ const Ranking = () => {
         score: `${ranking.score}`,
       };
     });
-    setTimeout(() => {
+    setTimeout(async () => {
       if (offset === 0) {
+        const rankingInformaion = await getRankingInformation();
+        setRankingCount(rankingInformaion.rankingCount);
+        setCurrentDateTime(rankingInformaion.currentTime);
         setTopRankingList(getTopRankingList(rankingList));
         setBottomRankingList(getBottomRankingList(rankingList));
       } else {
         setBottomRankingList([...bottomRankingList, ...rankingList]);
       }
+      if (isAllRankingsFetched()) {
+        /**
+         * 모든 랭킹 데이터를 가져왔을때, 버튼과 loading 컴포넌트를 제거하고
+         * api 요청은 lock을 걸어 놓음
+         */
+        setMoreButtomVisibiliy(false);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
       setMoreButtonLock(false);
     }, 500);
   };
@@ -63,12 +88,13 @@ const Ranking = () => {
         </Link>
       </Box>
       <TopRankPanel rankingList={topRankingList} />
-      <BottomRankPanel
-        rankingList={bottomRankingList}
-        loading={moreButtonLock}
-      />
+      <BottomRankPanel rankingList={bottomRankingList} loading={loading} />
       <Box className={classes.MoreButton}>
-        <MoreButton onClick={onClick}>MORE</MoreButton>
+        {moreButtomVisibiliy ? (
+          <MoreButton onClick={onClick}>MORE</MoreButton>
+        ) : (
+          ''
+        )}
       </Box>
     </Box>
   );
