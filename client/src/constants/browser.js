0284ae3c2d9130c2dc797b288940aba47c211f97
young na <ygna91@gmail.com>
2019-12-15 22:28:34 +0900

    Feature : 실행하면 url을 클립보드로 복사하는 함수 구현
    상세:
    - 함수를 실행하면 url을 보이지 않는 input 태그에 넣고 그것을 클립보드로 복사한다.
    - 이후 해당 element를 제거함
    - 매직 스트링 분리

---
+++ client/src/constants/browser.js
@@ -0,0 +1,4 @@
+const INPUT_TAG = 'input';
+const BROWSER_COPY_COMMAND = 'copy';
+
+export { INPUT_TAG, BROWSER_COPY_COMMAND };
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,3 +2,4 @@ export { default as CONSTANT_VALUES } from './constantValues';
 export { default as STYLE_COLORS } from './styleColors';
 export { default as browserLocalStorage } from './browserLocalStorage';
 export { default as makeViewPlayerList } from './makeViewPlayerList';
+export { default as copyUrlToClipoard } from './copyUrlToClipboard';
