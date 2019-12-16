2d71e087208216beff641a1c2f540e5eaf5c6773
young na <ygna91@gmail.com>
2019-12-16 13:03:48 +0900

    Refactor : share url 버튼 글을 제거하고 image를 삽입
    상세:
    - 이미지는 asset에 저장
    - share url 버튼으로서 재사용되는 목적으로서 img 태그를 컴포넌트에 넣음

--- client/src/presentation/components/Buttons/ShareUrlButton.jsx
+++ client/src/presentation/components/Buttons/ShareUrlButton.jsx
@@ -3,30 +3,29 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import buttonStyle from './style';
+import shareButtonImageSource from '../../../assets/share.png';
 
 const useStyles = makeStyles({
   button: {
     ...buttonStyle,
     width: '100%',
     height: '3.2rem',
+    '& img': {
+      width: '2rem',
+    },
   },
 });
 
-const ShareUrlButton = ({ onClick, children, classNames }) => {
+const ShareUrlButton = ({ onClick, classNames }) => {
   const classes = useStyles();
   return (
-    <Button
-      onClick={onClick}
-      variant="contained"
-      className={[...classNames, classes.button]}
-    >
-      {children}
+    <Button onClick={onClick} className={[...classNames, classes.button]}>
+      <img alt="share" src={shareButtonImageSource} />
     </Button>
   );
 };
 
 ShareUrlButton.propTypes = {
-  children: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   classNames: PropTypes.shape.isRequired,
 };
