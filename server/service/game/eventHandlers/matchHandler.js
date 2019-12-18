e4dd5013b95d813bdd7e1fa406e28ce86f3b8bcd
Mosball <rnjsrldnd123@naver.com>
2019-12-18 18:38:24 +0900

    Refactor : 주석 제거 및 코드 가독성 개선
    상세 :
    - if문 내의 주석 제거
    - if else문 이후의 emitEventsAfterJoin함수
    if문 안으로 이동
    
    이유 :
    - emitEventsAfterJoin함수는 if문이 true일 때만
    동작하는 코드이므로 가독성 개선을 위해 if문
    내부로 이동
    
    #502

--- server/service/game/eventHandlers/matchHandler.js
+++ server/service/game/eventHandlers/matchHandler.js
@@ -35,21 +35,16 @@ const matchHandler = (
   roomController.joinRoom({ socket, roomId, player, isRoomPrivate });
 
   if (roomId) {
-    /**
-     * 새로운 플레이어는 기존 플레이어의 정보들을 전달받고
-     * 기존의 플레이어들은 새로운 플레이어의 정보를 전달받는다.
-     */
     const room = roomController.getRoomByRoomId(roomId);
     const otherPlayers = room.gameManager.getOtherPlayers(player.socketId);
 
     socket.broadcast.to(roomId).emit('sendNewPlayer', player);
     socket.emit('sendPlayers', { players: otherPlayers });
+    emitEventsAfterJoin(socket);
   } else {
     /** @todo 토스터 완성되면 여기서 메인보내고 토스터로 없다고 알려줘야함 */
     socket.disconnect();
-    return;
   }
-  emitEventsAfterJoin(socket);
 };
 
 module.exports = matchHandler;
