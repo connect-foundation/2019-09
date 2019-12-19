6853e065823fb776f9fd053f4bb782570e3eaca0
JeongHyeonJo <zo13258@naver.com>
2019-12-19 16:24:57 +0900

    Feature : 플레이어 삽입 테스트 구현
    상세 :
    - player 객체 생성(위에 있는 player는 이후 스트리머로 재활용 할 예정)
    - players로 배열 생성(이후 viewer로 사용할 예정)

--- server/__test__/GameManager.test.js
+++ server/__test__/GameManager.test.js
@@ -1,8 +1,44 @@
 const GameManager = require('../service/game/models/GameManager');
+const Player = require('../service/game/models/Player');
 
 const roomId = 1;
 const gameManager = new GameManager(roomId);
 
 test('GameManager는 매개변수로 받은 roomId를 저장합니다.', () => {
   expect(gameManager.roomId).toBe(roomId);
+});
+
+const player = new Player({
+  socketId: 1,
+  nickname: 'player1',
+  nicknameColor: 'color',
+});
+
+const players = [];
+
+const insertPlayers = list => {
+  for (let i = 2; i < 5; i++) {
+    list.push(
+      new Player({
+        socketId: i,
+        nickname: `player${i}`,
+        nicknameColor: 'color',
+      }),
+    );
+  }
+};
+
+insertPlayers(players);
+
+test('GameManager에 플레이어를 넣습니다.', () => {
+  gameManager.addPlayer(player);
+  expect(gameManager.players[0]).toBe(player);
+  expect(gameManager.players[0]).toEqual(player);
+});
+
+test('GameManager에 여러 플레이어를 넣습니다.', () => {
+  players.forEach(eachPlayer => gameManager.addPlayer(eachPlayer));
+  players.forEach((eachPlayer, i) => {
+    expect(gameManager.players[i + 1]).toStrictEqual(eachPlayer);
+  });
 });
