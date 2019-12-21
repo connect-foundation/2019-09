c5675139a0ffce2e3f950882d4de575e4ca2266d
young na <ygna91@gmail.com>
2019-12-21 15:49:09 +0900

    Fix : 서버사이드 connectPeerHandler의 gameController.setIsConnectedToStreamer 매개변수 추가
    상세:
    - gameController.setIsConnectedToStreamer가 필요한 socket을 매개변수로 추가

--- server/service/game/eventHandlers/connectPeerHandler.js
+++ server/service/game/eventHandlers/connectPeerHandler.js
@@ -3,7 +3,7 @@ const gameController = require('../controllers/gameController');
 
 const connectPeerHandler = socket => {
   const { gameManager, timer } = roomController.getRoomByRoomId(socket.roomId);
-  gameController.setIsConnectedToStreamer(gameManager, true);
+  gameController.setIsConnectedToStreamer(gameManager, true, socket);
 
   /** 연결 준비 후 정상 시작 */
   if (gameController.isSetPreparable(gameManager)) {
